58776114f473c4736535e0e4cbc6d433a143a5c8
emsi06001 <emsi06001@E-PW0234N3>
2023-11-22 12:27:38 +0100

    Add project files.

---
+++ inluppg5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace inluppg5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[3];
+            nums[0] = 30;
+            nums[1] = 14;
+            Console.WriteLine("Skriv in ett tal");
+            nums[2] = int.Parse(Console.ReadLine());
+
+            Console.Write("Talen är: " + nums[0]);
+            for (int i  = 1; i < nums.Length; i++)
+            {
+                Console.Write(", " + nums[i]);
+            }
+        }
+    }
+}
